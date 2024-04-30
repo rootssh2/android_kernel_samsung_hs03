7e50882960bc3869106de97b265290cdba474655
Zillion <DevZillion@proton.me>
2024-04-30 19:41:08 +0300

    build_kernel.sh: automatically sync clang

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -d $(pwd)/toolchain/clang/host/linux-x86/clang-r416183b ]; then
+	mkdir -p $(pwd)/toolchain/clang/host/linux-x86/clang-r416183b
+	curl -sL https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-14.0.0_r0.68/clang-r416183b.tar.gz | tar -xzf - --directory $(pwd)/toolchain/clang/host/linux-x86/clang-r416183b
+fi
+
 export PATH="$(pwd)/toolchain/clang/host/linux-x86/clang-r416183b/bin:$PATH"
 export ARCH=arm64
 
