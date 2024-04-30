55693f1d3f443034f3a4abbefd303e6fe25c5770
Zillion <DevZillion@proton.me>
2024-04-30 16:30:38 +0300

    build_kernel.sh: build using clang r416183b

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
-export CLANG_TRIPLE=aarch64-linux-gnu-
+export PATH="$(pwd)/toolchain/clang/host/linux-x86/clang-r416183b/bin:$PATH"
 export ARCH=arm64
 
 
@@ -10,6 +8,5 @@ export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a03s_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
-
+make -C $(pwd) O=$(pwd)/out CROSS_COMPILE=aarch64-linux-gnu- CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM_IAS=1 KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
